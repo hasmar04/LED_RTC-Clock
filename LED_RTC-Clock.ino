cee3a0e3985957536d5c78a60d659a72d0ae25ee
Harrison Asmar <harrisonasmar@gmail.com>
2023-03-15 16:20:04 +1000

    Initial Version
    Finalises code to show date and time on matrix as obtained from RTC module.

--- LED_RTC-Clock.ino
+++ LED_RTC-Clock.ino
@@ -1,11 +1,45 @@
-#include <DS3231.h>
+//LED_RTC-Clock by Harrison Asmar
 
+// To set the time, enter it into the serial monitor in the order YYMMDDwHHMMSS, with an 'x' at the end
+
+//                  LED MATRIX
+/*-----------------------------------------------------------------------
+Only 7 of the 16 pins on the DMD are actually used:
+GND - Hopefully obvious
+nOE - Enable / Disable ALL the LEDs on the DMD
+A - A&B pins select which 1/4 of the DMD is selected
+B - A&B pins select which 1/4 of the DMD is selected
+CLK - Used to clock each pixel into the DMD shift registers
+SCLK - Latches the current content of the DMD shift registers
+R - The raw pixel data stream (NB: The 'R' stands for RED. You might notice an unused 'G' pin which is for
+Green on a HUB12 connector)
+--------
+nOE(D9) |1 2| A(D6)
+Gnd |3 4| B(D7)
+Gnd |5 6| C
+Gnd |6 8| CLK(D13)
+Gnd |7 10| SCLK(D8)
+Gnd |11 12| R(D11)
+Gnd |13 14| G
+Gnd |15 16| D
+--------------------------------------------------------------------------------*/
+
+#include <DS3231.h>
+#include "SPI.h"
+#include "DMD.h"
+#include "TimerOne.h"
+#include "SystemFont5x7.h"
+
+//Declare RTC function
 DS3231 RTC;
 
 //booleans for printing time
 bool CentBit;
 bool hr;
 bool PM;
+
+//Global array for RTCdate, RTCtime
+String Array[2];
 
 //vars for setting time
 byte year;
@@ -16,11 +50,7 @@ byte hour;
 byte minute;
 byte second;
 
-void getDateStuff(byte& year, byte& month, byte& date, byte& dOW,
-                  byte& hour, byte& minute, byte& second) {
-  // Call this if you notice something coming in on
-  // the serial port. The stuff coming in should be in
-  // the order YYMMDDwHHMMSS, with an 'x' at the end.
+void getDateStuff(byte& year, byte& month, byte& date, byte& dOW, byte& hour, byte& minute, byte& second) {  //Function for parsing time data collected from the serial monitor so it can be used to set the RTC.
   boolean gotString = false;
   char inChar;
   byte temp1, temp2;
@@ -66,7 +96,7 @@ void getDateStuff(byte& year, byte& month, byte& date, byte& dOW,
   second = temp1 * 10 + temp2;
 }
 
-void funcRTC() { //function that contains all RTC functions
+String funcRTC() {             //function that contains all RTC functions
   if (Serial.available()) {  //if data is entered into the serial monitor, parse it as a time update
     getDateStuff(year, month, date, dOW, hour, minute, second);
 
@@ -79,31 +109,86 @@ void funcRTC() { //function that contains all RTC functions
     RTC.setHour(hour);
     RTC.setMinute(minute);
     RTC.setSecond(second);
+
+    //Print time that has been set
+    Serial.print('The following time has been saved to the RTC: ');
+    Serial.print(RTC.getDoW(), DEC);
+    Serial.print(' ');
+    Serial.print(RTC.getYear(), DEC);
+    Serial.print('/');
+    Serial.print(RTC.getMonth(CentBit), DEC);
+    Serial.print('/');
+    Serial.print(RTC.getDate(), DEC);
+    Serial.print(' ');
+    Serial.print(RTC.getHour(hr, PM), DEC);
+    Serial.print(':');
+    Serial.print(RTC.getMinute(), DEC);
+    Serial.print(':');
+    Serial.print(RTC.getSecond(), DEC);
+    Serial.println();
   }
 
+  String RTCdate,RTCtime;
+
+
   //Print the date and time output by the RTC
-  Serial.print(RTC.getDoW(), DEC);
-  Serial.print(' ');
-  Serial.print(RTC.getYear(), DEC);
-  Serial.print('/');
-  Serial.print(RTC.getMonth(CentBit), DEC);
-  Serial.print('/');
-  Serial.print(RTC.getDate(), DEC);
-  Serial.print(' ');
-  Serial.print(RTC.getHour(hr, PM), DEC);
-  Serial.print(':');
-  Serial.print(RTC.getMinute(), DEC);
-  Serial.print(':');
-  Serial.print(RTC.getSecond(), DEC);
-  Serial.println();
+  RTCdate=/*String(RTC.getYear())+'/'+*/String(RTC.getMonth(CentBit))+'/'+String(RTC.getDate());
+  Serial.println(RTCdate);
+  RTCtime=String(RTC.getHour(hr,PM))+':'+String(RTC.getMinute())/*+':'+String(RTC.getSecond())*/;
+  Serial.println(RTCtime);
+  Array[0] = RTCdate;
+  Array[1] = RTCtime;
+}
+
+// LED MATRIX SETUP
+
+//Fire up the DMD library as dmd
+#define DISPLAYS_ACROSS 1
+#define DISPLAYS_DOWN 1
+DMD dmd(DISPLAYS_ACROSS, DISPLAYS_DOWN);
+
+void ScanDMD() {
+  dmd.scanDisplayBySPI();
+}
+
+const byte PanelWidth = 32;
+const byte MaxStringLength = 5;
+char CharBuf[MaxStringLength + 1];
+
+void center_theDisplay(String input_Str,int y_position) {
+  byte charCount, total_charWidth, x_position;
+  input_Str.toCharArray(CharBuf, MaxStringLength + 1);  //string to char array
+  charCount = input_Str.length();
+  if (charCount == 0) exit;
+  total_charWidth = 0;
+  for (byte thisChar = 0; thisChar < charCount; thisChar++) {
+    total_charWidth = total_charWidth + dmd.charWidth(CharBuf[thisChar]) + 1;  //add 1 pixel for space
+  }
+  total_charWidth = total_charWidth - 1;            //no space for last letter
+  x_position = (PanelWidth - total_charWidth) / 2;  //position(x) of first letter
+  for (byte thisChar = 0; thisChar < charCount; thisChar++) {
+    //dmd.drawChar(x, y,‘@', GRAPHICS_NORMAL)
+    dmd.drawChar(x_position, y_position, CharBuf[thisChar], GRAPHICS_NORMAL);
+    x_position = x_position + dmd.charWidth(CharBuf[thisChar]) + 1;  //position for next letter
+  }
 }
 
 void setup() {
+  //Initialise Wire for RTC and Serial for debugging
   Wire.begin();
   Serial.begin(9600);
+
+  //initialize TimerOne's interrupt/CPU usage used to scan and refresh the display
+  Timer1.initialize(5000);          //period in microseconds to call ScanDMD. Anything longer than 5000 (5ms) and you can see flicker.
+  Timer1.attachInterrupt(ScanDMD);  //attach the Timer1 interrupt to ScanDMD which goes to dmd.scanDisplayBySPI()
+  dmd.selectFont(SystemFont5x7);
 }
 
 void loop() {
   delay(1000);
+  //String RTC[] = funcRTC();
   funcRTC();
+  dmd.clearScreen(true);
+  center_theDisplay(Array[0],0);
+  center_theDisplay(Array[1],8);
 }
